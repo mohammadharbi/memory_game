3d1ec67fadcda7ca9f7e09b325fe603f55eff185
mohammadharbi <mohammad.a.aljagthmi@gmail.com>
2020-02-16 18:58:44 +0300

    NEW MESSAGE
    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+let cardOne = "king";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree); 
