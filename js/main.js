a8825ad7a360cecec19ee7f383a3ad0c4713af43
mohammadharbi <mohammad.a.aljagthmi@gmail.com>
2020-02-16 23:02:07 +0300

    NEW MESSAGE
    created logic game.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-console.log("Up and running!");
 
-let cardOne = "king";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "queen";
+const cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree); 
+const cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+
+if(cardsInPlay.length === 2)
+{
+	if(cardsInPlay[0] === cardsInPlay[1])
+	{
+		alert("You found a match!");
+
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
